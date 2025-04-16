8748b79619eedb8f937320cc1ebc1527bcfee357
ErwinMulia <erwinjuice098@gmail.com>
2025-04-16 19:24:06 +0800

    update

--- src/components/home/HowItWorks.tsx
+++ src/components/home/HowItWorks.tsx
@@ -1,11 +1,8 @@
 import { HowItWorks } from '@/components/organism/HowItWork';
-import { HeroSection } from '@/components/home/herosection';
 
 export default function HomePage() {
   return (
     <main>
-      <HeroSection />
-      
       <HowItWorks />
     </main>
   );
